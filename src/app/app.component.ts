13b83efff878ca90b0298bd071bfc31e0671a603
unknown <cplascencia21@yahoo.com>
2018-06-06 15:44:57 -0700

    string-interpolation2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `
+  //   <p>The sum is: {{ first + second }}</p>
+  // `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  first = 21;
+
+  second = 36;
+
+  checkLargest() {
+    return Math.max(this.first, this.second);
+  }
+
 }
